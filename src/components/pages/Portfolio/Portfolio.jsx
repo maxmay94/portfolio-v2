60ca110337b153d0b6c7977af57215d5e11eddf9
maxmay94 <maxmay94@gmail.com>
2022-04-20 12:28:33 -0400

    add portfolio images and links

--- src/components/pages/Portfolio/Portfolio.jsx
+++ src/components/pages/Portfolio/Portfolio.jsx
@@ -1,40 +1,40 @@
 import React from "react";
-import image1 from "../../../images/portfolio/project-01.jpg";
-import image2 from "../../../images/portfolio/project-02.jpg";
-import image3 from "../../../images/portfolio/project-03.jpg";
-import image4 from "../../../images/portfolio/project-04.jpg";
+import image1 from "../../../images/portfolio/art-critic.png";
+import image2 from "../../../images/portfolio/trouvaille.png";
+import image3 from "../../../images/portfolio/mtg-deck-builder.png";
+import image4 from "../../../images/portfolio/wordle-2.png";
 import PortfolioItem from "./PortfolioItem";
 
 const portfolioData = [
   {
     id: 1,
     image: image1,
-    title: "Space Bootstrap 5 Multipurpose Template",
-    link: "#0",
+    title: "art critic.",
+    link: "https://art-critic.netlify.app/",
     description:
       "Lorem ipsum dolor sit aconsect dipisicing elit, sed do eiusmod to incididunt uabore etdolore magna aliqua.",
   },
   {
     id: 2,
     image: image2,
-    title: "eCommerce HTML Bootstrap 5 UI Kit",
-    link: "#0",
+    title: "Trouvaille",
+    link: "https://trouvaille-by-wanderlust.netlify.app/",
     description:
       "Lorem ipsum dolor sit aconsect dipisicing elit, sed do eiusmod to incididunt uabore etdolore magna aliqua.",
   },
   {
     id: 3,
     image: image3,
-    title: "Basic – Tailwind CSS Template",
-    link: "#0",
+    title: "MTG Deck Builder",
+    link: "https://deck-maker-mtg.herokuapp.com/",
     description:
       "Lorem ipsum dolor sit aconsect dipisicing elit, sed do eiusmod to incididunt uabore etdolore magna aliqua.",
   },
   {
     id: 4,
     image: image4,
-    title: "Gearix Car repair App",
-    link: "#0",
+    title: "Wordle 2",
+    link: "https://maxmay94.github.io/wordle2/",
     description:
       "Lorem ipsum dolor sit aconsect dipisicing elit, sed do eiusmod to incididunt uabore etdolore magna aliqua.",
   },
