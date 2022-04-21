00d32ac093d22774a6843540c5b1a79b8996471a
maxmay94 <maxmay94@gmail.com>
2022-04-20 20:14:05 -0400

    deployment fix

--- src/components/pages/Service/ServiceItem.jsx
+++ src/components/pages/Service/ServiceItem.jsx
@@ -5,7 +5,7 @@ const ServiceItem = (props) => {
   return (
     <div className="w-full lg:w-1/2">
       <div className="my-4 md:mx-4 shadow p-6 rounded-md bg-gray-900 group hover:shadow-md">
-        <div className="w-16 h-16 flex items-center justify-center rounded-md text-3xl mb-5 bg-gray-100 text-green-600 transition duration-200 group-hover:bg-green-600 group-hover:text-white">
+        <div className="w-16 h-16 flex items-center justify-center rounded-md text-3xl mb-5 bg-gray-100 text-yellow-600 transition duration-200 group-hover:bg-green-600 group-hover:text-white">
           {icon}
         </div>
         <h3 className="text-lg font-medium text-gray-200 mb-2">{title}</h3>
