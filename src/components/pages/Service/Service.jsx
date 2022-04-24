ac65f3f74b9e3d74181c2129fa9588266034a0d2
maxmay94 <maxmay94@gmail.com>
2022-04-24 16:12:53 -0400

    change skills page

--- src/components/pages/Service/Service.jsx
+++ src/components/pages/Service/Service.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { FaCode, FaReact, FaPython, FaDatabase, FaJsSquare } from "react-icons/fa"
+import { FaReact, FaPython, FaJsSquare, FaHtml5, FaCss3, FaNodeJs, FaJava } from "react-icons/fa"
 import { SiTailwindcss } from "react-icons/si"
 import ServiceItem from "./ServiceItem"
 
@@ -7,46 +7,34 @@ const serviceData = [
   {
     id: 1,
     icon: <FaJsSquare />,
-    title: "JavaScript",
-    description:
-      "My Language of choice. JavaScript is easy to work with and quick to get from idea to a finished product. Its ability to adapt to whatever you could want to do with it makes it hard not to love.",
+    icon2: <FaPython />,
+    icon3: <FaHtml5 />,
+    icon4: <FaCss3/>,
+    icon5: <FaJava/>,
+    icon6: <i class="devicon-processing-plain-wordmark"></i>,
+    title: "Languages",
   },
   {
     id: 2,
     icon: <FaReact />,
-    title: "React / Express",
-    description:
-      "React really is incredible! There are, of course, many other libraries and frameworks I'd love to learn, but React seems to be limitless.",
+    icon2: <i class="devicon-flask-original"></i>,
+    icon3: <FaNodeJs />,
+    icon4: <i class="devicon-express-original"></i>,
+    icon5: <SiTailwindcss />,
+    icon6: <i class="devicon-bootstrap-plain"></i>,
+    title: "Frameworks / Libraries",
   },
   {
     id: 3,
-    icon: <SiTailwindcss />,
-    title: "Tailwind / Bootstrap CSS ",
-    description:
-      "Tailwind is my go-to for styling. I'm well acquainted with Bootstrap and vanilla CSS as well, but I love the simple elegance of Tailwind.",
+    icon: <i class="devicon-mongodb-plain"></i>,
+    icon2: <i class="devicon-postgresql-plain"></i>,
+    icon3: <i class="devicon-arduino-plain"></i>,
+    icon4: <i class="devicon-bash-plain"></i>,
+    icon5: <i class="devicon-github-original"></i>,
+    icon6: <i class="devicon-vscode-plain"></i>,
+    title: "Miscellaneous",
   },
-  {
-    id: 4,
-    icon: <FaPython />,
-    title: "Python",
-    description:
-      "Python is an incredibly simple and powerful language. I'm excited to get to know it better and experiment with TensorFlow and other interesting libraries.",
-  },
-  {
-    id: 5,
-    icon: <FaCode />,
-    title: "REST / CRUD",
-    description:
-      "The internet is built on RESTful foundations and we interact with it using CRUD. These two concepts are paramount and their importance can’t be overstated.",
-  },
-  {
-    id: 6,
-    icon: <FaDatabase />,
-    title: "Mongo DB / PostgreSQL",
-    description:
-      "The ability to perform CRUD operations on persistent data makes the internet a much more interesting and usable place.",
-  },
-];
+]
 
 const Service = () => {
   return (
--- src/components/pages/Service/ServiceItem.jsx
+++ src/components/pages/Service/ServiceItem.jsx
@@ -1,15 +1,55 @@
 import React from "react"
 
 const ServiceItem = (props) => {
-  const { icon, title, description } = props.service;
+  const { icon, icon2, icon3, icon4, icon5, icon6, title } = props.service;
   return (
     <div className="w-full lg:w-1/2">
-      <div className="my-4 md:mx-4 shadow p-6 rounded-md bg-gray-900 group hover:shadow-md">
-        <div className="w-16 h-16 flex items-center justify-center rounded-md text-3xl mb-5 bg-gray-100 text-yellow-600 transition duration-200 group-hover:bg-green-600 group-hover:text-white">
-          {icon}
+      <h3 className="text-lg m-5 font-medium text-gray-200 mb-2">{title}</h3>
+
+      <div className="my-4 md:mx-4 flex shadow p-6 rounded-md bg-gray-900 group hover:shadow-md items-center justify-center">
+        <div>
+          {
+            icon &&
+            <div className="w-16 h-16 flex items-center justify-center rounded-md text-3xl m-5 mb-5 bg-gray-100 text-yellow-600 transition duration-200 group-hover:bg-green-600 group-hover:text-white">
+              {icon}
+            </div>
+          }
+          {
+            icon2 &&
+            <div className="w-16 h-16 flex items-center justify-center rounded-md text-3xl m-5 mb-5 bg-gray-100 text-yellow-600 transition duration-200 group-hover:bg-green-600 group-hover:text-white">
+              {icon2}
+            </div>
+          }
         </div>
-        <h3 className="text-lg font-medium text-gray-200 mb-2">{title}</h3>
-        <p className="text-gray-400">{description}</p>
+        <div>
+          {
+            icon3&&
+            <div className="w-16 h-16 flex items-center justify-center rounded-md text-3xl m-5 mb-5 bg-gray-100 text-yellow-600 transition duration-200 group-hover:bg-green-600 group-hover:text-white">
+              {icon3}
+            </div>
+          }
+          {
+            icon4 &&
+            <div className="w-16 h-16 flex items-center justify-center rounded-md text-3xl m-5 mb-5 bg-gray-100 text-yellow-600 transition duration-200 group-hover:bg-green-600 group-hover:text-white">
+              {icon4}
+            </div>
+          }
+          
+        </div>
+        <div>
+          {
+            icon5 &&
+            <div className="w-16 h-16 flex items-center justify-center rounded-md text-3xl m-5 mb-5 bg-gray-100 text-yellow-600 transition duration-200 group-hover:bg-green-600 group-hover:text-white">
+              {icon5}
+            </div>
+          }
+          {
+            icon6 &&
+            <div className="w-16 h-16 flex items-center justify-center rounded-md text-3xl m-5 mb-5 bg-gray-100 text-yellow-600 transition duration-200 group-hover:bg-green-600 group-hover:text-white">
+              {icon6}
+            </div>
+          }
+        </div>
       </div>
     </div>
   )
